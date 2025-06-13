538d63a28ed26f58744daa42f2891ed528ebb176
TakissAz <lpze.yesenia.azfrith.blanco.ma@unifranz.edu.bo>
2025-06-13 16:42:37 -0400

    lo que sea

--- scenes/empresarie.gd
+++ scenes/empresarie.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
+const SPEED: float = 100.0
+var motion: Vector2 = Vector2.ZERO
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation: AnimationPlayer = $AnimationPlayer
 
+func _physics_process(_delta: float) -> void:
+	motion = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		animation.play("run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		animation.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
+	velocity = motion
 	move_and_slide()
